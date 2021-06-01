4255475fd9aa1f2ebe97f2337376a9cea8b939d1
Egor Belyaev <belyaev.eda@gmail.com>
2021-06-01 22:05:55 +0300

    CORS privacy

---
+++ src/main/java/my/egorrridze/config/corsConfig.java
@@ -0,0 +1,26 @@
+package my.egorrridze.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+@Configuration
+public class corsConfig {
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
